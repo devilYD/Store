029693924c0f0a408d64dfdda9e3401a971b9977
devilYD <devilyudun@gmail.com>
2022-11-30 15:29:56 +0800

    创建项目

---
+++ store_pojo/src/test/java/org/yd/StorePojoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorePojoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ store_web/src/main/java/org/yd/StoreWebApplication.java
@@ -0,0 +1,13 @@
+package org.yd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreWebApplication.class, args);
+    }
+
+}
