886d09a86378ef26d6fde17e32aee4d1e11a6de0
devilYD <devilyudun@gmail.com>
2022-12-06 12:34:28 +0800

    尝试在无启动类的情况下手动启动IOC容器进行单元测试

--- README.md
+++ README.md
@@ -27,4 +27,25 @@
   * store_service：业务层
   * store_web：控制层以及前端代码
 
+## 数据库表结构
 
+### 用户表
+
+```SQL
+CREATE TABLE t_user (
+	uid INT AUTO_INCREMENT COMMENT '用户id',
+	username VARCHAR(20) NOT NULL UNIQUE COMMENT '用户名',
+	password CHAR(32) NOT NULL COMMENT '密码',
+	salt CHAR(36) COMMENT '盐值',
+	phone VARCHAR(20) COMMENT '电话号码',
+	email VARCHAR(30) COMMENT '电子邮箱',
+	gender INT COMMENT '性别:0-女，1-男',
+	avatar VARCHAR(50) COMMENT '头像',
+	is_delete INT COMMENT '是否删除：0-未删除，1-已删除',
+	created_user VARCHAR(20) COMMENT '日志-创建人',
+	created_time DATETIME COMMENT '日志-创建时间',
+	modified_user VARCHAR(20) COMMENT '日志-最后修改执行人',
+	modified_time DATETIME COMMENT '日志-最后修改时间',
+	PRIMARY KEY (uid)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+```
--- store_dao/src/test/java/org/yd/StoreDaoApplicationTests.java
+++ store_dao/src/test/java/org/yd/StoreDaoApplicationTests.java
@@ -1,13 +1,25 @@
 package org.yd;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class StoreDaoApplicationTests {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.yd.config.MyBatisPlusConfig;
+import org.yd.dao.UserDao;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = MyBatisPlusConfig.class,loader = AnnotationConfigContextLoader.class)
+public class StoreDaoApplicationTests {
+
+    @Autowired
+    private UserDao userDao;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        System.out.println(userDao.selectList(null));
     }
 
 }
